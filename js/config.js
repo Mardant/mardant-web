825d72473135bea45304999444acb2ee75e7e00d
Mardant <45545900+Mardant@users.noreply.github.com>
2025-04-22 15:01:29 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // === js/config.js ===
 // Pon aquí la URL de tu Apps Script desplegado (“Acceso anónimo”)
-export const API_URL = 'https://script.google.com/macros/s/AKfycbysPZ-8i_7WVuFonzsC3RjnsSkZrsU3ossJWGV1WfaOZrm5Qv9jfy2yTWltvvkkzWDE/exec';
+export const API_URL = 'https://script.google.com/macros/s/AKfycbw76Zm4srmYuv0djwvTInKotDtZ3_EXDeN75Y4_rQnWMxVMoAJ7cTznduTGvMQyJ_qE/exec';
