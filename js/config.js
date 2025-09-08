a231156da78d353bb2d01f437d3e339dd5a4b4e5
Mardant <45545900+Mardant@users.noreply.github.com>
2025-09-08 00:47:48 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 // === js/config.js ===
-export const API_URL = 'https://script.google.com/macros/s/AKfycbzi5G-NDlshHRO0c2uOEPNceFS2FVxHLHI2i2xra1ODryxRHN4LRlWnteayBN0pkjIG/exec';
+export const API_URL = 'https://script.google.com/macros/s/AKfycbwyYBmqhFJSTzyGG22IF2OaUQlbImTeTZY3WzBBngaDC8qU0mCBzEyXqesQyK-J1J3m/exec';
 export const CARRITO_LOCAL_KEY = 'carritoMardant';
 export const PRODUCTOS_POR_PAGINA = 21;
 
 // NUEVO: claves usadas por Mi Cuenta
 export const AUTH_KEYS = { TOKEN:'mardant_token', CLIENT:'mardant_client', NAME:'mardant_name' };
+
 
