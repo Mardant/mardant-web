26366ad2f2d8e3f4e08d603a126c3f50aa6b1936
Mardant <45545900+Mardant@users.noreply.github.com>
2025-09-08 00:29:37 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 // === js/config.js ===
 // Pon aquí la URL de tu Apps Script desplegado (“Acceso anónimo”)
-export const API_URL = 'https://script.google.com/macros/s/AKfycbw76Zm4srmYuv0djwvTInKotDtZ3_EXDeN75Y4_rQnWMxVMoAJ7cTznduTGvMQyJ_qE/exec';
+export const API_URL = 'https://script.google.com/macros/s/AKfycbzi5G-NDlshHRO0c2uOEPNceFS2FVxHLHI2i2xra1ODryxRHN4LRlWnteayBN0pkjIG/exec';
 export const CARRITO_LOCAL_KEY = 'carritoMardant';
 export const PRODUCTOS_POR_PAGINA = 21;
+
