2fe8716e05292d269876d0de9ad41cd8e2a95768
Mardant <45545900+Mardant@users.noreply.github.com>
2025-09-08 00:37:02 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
 // === js/config.js ===
-// Pon aquí la URL de tu Apps Script desplegado (“Acceso anónimo”)
 export const API_URL = 'https://script.google.com/macros/s/AKfycbzi5G-NDlshHRO0c2uOEPNceFS2FVxHLHI2i2xra1ODryxRHN4LRlWnteayBN0pkjIG/exec';
 export const CARRITO_LOCAL_KEY = 'carritoMardant';
 export const PRODUCTOS_POR_PAGINA = 21;
+
+// NUEVO: claves usadas por Mi Cuenta
+export const AUTH_KEYS = { TOKEN:'mardant_token', CLIENT:'mardant_client', NAME:'mardant_name' };
 
